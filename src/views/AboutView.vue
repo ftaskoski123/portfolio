c4e2af37f7430ab6bce73d3d6f4df5f616006122
ftaskoski123 <filip.taskoski69@gmail.com>
2023-09-14 18:58:10 +0200

    2xl device test project

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
   <!-- card look -->
-  <div class="my-8 mx-auto p-6 bg-gray-100 rounded-xl">
+  <div class="my-8 mx-auto p-6 2xl:flex 2xl:mx-16 bg-gray-100 rounded-xl">
     <!-- project photo -->
     <div
-      class="flex justify-center bg-white items-center rounded-2xl shadow-md"
+      class="flex justify-center 2xl:w-[550px] bg-white items-center rounded-2xl shadow-md"
     >
       <div
         class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
       ></div>
     </div>
-
+    
     <p class="text-black text-center">Find A Coach</p>
   </div>
 
