6c66cc9484eab093643cd9e3ac7a5e94595ccae3
Filip Tashkoski <filip.taskoski69@gmail.com>
2023-09-14 11:39:44 +0200

    fixi

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -20,21 +20,7 @@
       class="flex justify-center items-center rounded-2xl bg-white shadow-md"
     >
       <div
-        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom "
-      ></div>
-    </div>
-
-    <p class="text-black text-center">Find A Coach</p>
-  </div>
-
-  <!-- card look -->
-  <div class="my-8 mx-auto p-6 bg-gray-100 rounded-xl">
-    <!-- project photo -->
-    <div
-      class="flex justify-center items-center rounded-2xl bg-white dark:bg-black shadow-md"
-    >
-      <div
-        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom "
+        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
       ></div>
     </div>
 
@@ -48,7 +34,21 @@
       class="flex justify-center items-center rounded-2xl bg-white shadow-md"
     >
       <div
-        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom "
+        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
+      ></div>
+    </div>
+
+    <p class="text-black text-center">Find A Coach</p>
+  </div>
+
+  <!-- card look -->
+  <div class="my-8 mx-auto p-6 bg-gray-100 rounded-xl">
+    <!-- project photo -->
+    <div
+      class="flex justify-center items-center rounded-2xl bg-white shadow-md"
+    >
+      <div
+        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
       ></div>
     </div>
 
