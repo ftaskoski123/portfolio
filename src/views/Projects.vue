abd78da048fbcf9741658ecfd1a6665992a3def5
ftaskoski123 <filip.taskoski69@gmail.com>
2023-09-17 00:26:38 +0200

    find a coach project display

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="px-4 mt-[65rem] mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+
   <!-- card look -->
-  <div class="my-8 mx-auto p-6 2xl:flex 2xl:mx-16 bg-gray-100 rounded-xl">
+  <div class="my-8 mx-auto p-6 2xl:flex 2xl:mx-16  bg-gray-100 rounded-xl">
     <!-- project photo -->
     <div
-      class="flex justify-center 2xl:w-[550px] bg-white items-center rounded-2xl shadow-md"
+      class="flex justify-center 2xl:w-[550px]  items-center rounded-2xl shadow-md"
     >
       <div
-        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
+        class="h-[350px] w-[550px] rounded-2xl bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
+      ></div>
+    </div>
+    
+    <div class="text-center text-black 2xl:w-full 2xl:text-left">
+    <h3 class="font-black mt-4 2xl:mt-0 text-lg 2xl:text-md 2xl:ml-44">FIND A COACH 🧑‍🏫</h3>
+    <p class="2xl:mt-10 2xl:mx-6 text-base font-bold leading-[1.6] text-gray-500">
+      Welcome to the "Find a Coach" project, a platform where users can search for coaches in various fields such as frontend development, backend development, and career guidance. Whether you're looking to learn, improve your skills, or seek guidance in your career, this app helps you connect with experienced coaches.
+    </p>
+    <div class="flex gap-[1rem] justify-center mt-[1rem]">
+      <p class="border font-semibold  shadow-lg py-2 px-6">Vue</p>
+      <p class="border font-semibold  shadow-lg py-2 px-6">Firebase</p>
+  </div>
+  </div>
+
+  </div>
+
+  <!-- card look -->
+  <div class="my-8 mx-auto p-6 2xl:flex 2xl:mx-16  bg-gray-100 rounded-xl">
+    <!-- project photo -->
+    <div
+      class="flex justify-center 2xl:w-[550px]  items-center rounded-2xl shadow-md"
+    >
+      <div
+        class="h-[350px] w-[550px] rounded-2xl bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
       ></div>
     </div>
     
@@ -15,52 +40,38 @@
   </div>
 
   <!-- card look -->
-  <div class="my-8 mx-auto p-6 bg-gray-100 rounded-xl">
+  <div class="my-8 mx-auto p-6 2xl:flex 2xl:mx-16  bg-gray-100 rounded-xl">
     <!-- project photo -->
     <div
-      class="flex justify-center items-center rounded-2xl bg-white shadow-md"
+      class="flex justify-center 2xl:w-[550px]  items-center rounded-2xl shadow-md"
     >
       <div
-        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
+        class="h-[350px] w-[360px] rounded-2xl bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
       ></div>
     </div>
-
+    
     <p class="text-black text-center">Find A Coach</p>
   </div>
 
   <!-- card look -->
-  <div class="my-8 mx-auto p-6 bg-gray-100 rounded-xl">
+  <div class="my-8 mx-auto p-6 2xl:flex 2xl:mx-16  bg-gray-100 rounded-xl">
     <!-- project photo -->
     <div
-      class="flex justify-center items-center rounded-2xl bg-white shadow-md"
+      class="flex justify-center 2xl:w-[550px]  items-center rounded-2xl shadow-md"
     >
       <div
-        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
+        class="h-[350px] w-[360px] rounded-2xl bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
       ></div>
     </div>
-
+    
     <p class="text-black text-center">Find A Coach</p>
   </div>
-
-  <!-- card look -->
-  <div class="my-8 mx-auto p-6 bg-gray-100 rounded-xl">
-    <!-- project photo -->
-    <div
-      class="flex justify-center items-center rounded-2xl bg-white shadow-md"
-    >
-      <div
-        class="h-[350px] w-[300px] bg-black transition-transform hover-transition ease-in-out bg-[url('@/assets/ss.png')] bg-cover bg-top hover:bg-bottom"
-      ></div>
     </div>
-
-    <p class="text-black text-center">Find A Coach</p>
-  </div>
-  
-</div>
 </template>
 
 <style scoped>
 .hover-transition {
   transition: 5s;
 }
+
 </style>
