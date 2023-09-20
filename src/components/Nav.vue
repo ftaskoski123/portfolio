8cfa5dd2a13ec0d8d66db8d19cbaabd5073fae9e
ftaskoski123 <filip.taskoski69@gmail.com>
2023-09-20 20:00:32 +0200

    changed the light/dark mode svg's

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,52 +1,73 @@
 <template>
   <div class="flex justify-center w-full">
     <header
-      class="p-5 shadow-md bg-white dark:bg-gray-800  fixed z-50 md:mr-0 md:flex md:items-center md:justify-between w-full"
+      class="p-5 shadow-md bg-white dark:bg-gray-800 fixed z-50 md:mr-0 md:flex md:items-center md:justify-between w-full"
     >
       <div class="flex justify-between items-center">
-        <span
-          class="text-3xl cursor-pointer mx-2 md:hidden block"
-        >
-        </span>
+        <span class="text-3xl cursor-pointer mx-2 md:hidden block"> </span>
       </div>
       <button
         @click="toggleDark()"
-        class="px-2 py-2 text-white shadow-md transition-transform duration-500 ease-in-out hover:scale-105 bg-gray-700 rounded-full dark:bg-white absolute top-3 right-4"
+        class="px-2 py-2 text-white shadow-md transition-transform duration-500 ease-in-out hover:scale-105 bg-gray-700 dark:bg-white rounded-full absolute top-3 right-4"
       >
         <span v-if="dark">
-          <img src="../assets/sun.png" alt="Sun Icon" class="rounded w-8 h-8" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 text-gray-800"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            />
+          </svg>
         </span>
         <span v-else>
-          <img
-            src="../assets/moon.png"
-            alt="Moon Icon"
-            class="rounded w-8 h-8"
-          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>
         </span>
       </button>
-      <ul class="hidden md:flex text-black text-xl mr-10  dark:text-white">
+      <ul class="hidden md:flex text-black text-xl mr-10 dark:text-white">
         <li class="mx-4 my-6 md:my-0">
-          <router-link to=""
+          <router-link
+            to=""
             @click="scrollToTop"
             class="hover:text-blue-500 cursor-pointer duration-500 focus:text-blue-500"
             >HOME</router-link
           >
         </li>
         <li class="mx-4 my-6 md:my-0">
-          <router-link to=""
+          <router-link
+            to=""
             @click="scrollToProjects"
             class="hover:text-blue-500 cursor-pointer duration-500 focus:text-blue-500"
             >PROJECTS</router-link
           >
         </li>
         <li class="mx-4 my-6 md:my-0">
-          <router-link to=""
+          <router-link
+            to=""
             @click="scrollToContact"
             class="hover:text-blue-500 cursor-pointer duration-500 focus:text-blue-500"
             >CONTACT</router-link
           >
         </li>
- 
       </ul>
 
       <!--Phone-->
@@ -64,21 +85,24 @@
           class="md:flex mt-5 md:items-center z-10 md:z-auto md:static fixed text-black bg-white w-screen h-screen left-0 md:w-auto py-2 pl-1 md:opacity-100 md:rounded-none md:mt-0"
         >
           <li class="mx-4 my-6 md:my-0">
-            <router-link to=""
+            <router-link
+              to=""
               @click="scrollToTopMobile"
               class="text-xl focus:text-blue-500 hover:text-blue-500 duration-500"
               >HOME</router-link
             >
           </li>
           <li class="mx-4 my-6 md:my-0">
-            <router-link to=""
+            <router-link
+              to=""
               @click="scrollToProjectsMobile"
               class="text-xl focus:text-blue-500 hover:text-blue-500 duration-500"
               >PROJECTS</router-link
             >
           </li>
           <li class="mx-4 my-6 md:my-0">
-            <router-link to=""
+            <router-link
+              to=""
               @click="scrollToContactMobile"
               class="text-xl focus:text-blue-500 hover:text-blue-500 duration-500"
               >CONTACT</router-link
@@ -119,7 +143,7 @@ function scrollToProjects() {
 }
 
 function scrollToTop() {
-  window.scrollTo({ top: 0,left: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   router.push({ hash: "#home" });
 }
 
@@ -143,8 +167,8 @@ function closeLinksOnResize() {
   }
 }
 function scrollToProjectsMobile(): void {
-  scrollToProjects(); 
-  toggleLinksVisibility(); 
+  scrollToProjects();
+  toggleLinksVisibility();
 }
 
 function scrollToContactMobile(): void {
